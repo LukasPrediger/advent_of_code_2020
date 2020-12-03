dbdaa2c3fc0d36809e3c58e4e81089ee92f1d81f
Lukas Prediger <lukas.prediger@rewe-digital.com>
2020-12-03 13:27:30 +0100

    Day3 part2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -5,30 +5,39 @@ fn main() {
     let file = File::open("input.txt").expect("Error opening file");
     let lines = BufReader::new(file).lines();
 
-    let parsed_lines: Vec<Vec<i32>> = lines.map(|line| {
-        line.unwrap().chars().map(|char| return if char == '#' { 1 } else { 0 }).collect()
+    let parsed_lines: Vec<Vec<usize>> = lines.map(|line| {
+        line.unwrap().chars().map(|char| return if char == '#' { 1usize } else { 0usize }).collect()
     }
     ).collect();
 
+    let slopes: [(usize, usize);5] = [(1,1), (3,1),(5,1), (7,1), (1,2)];
+
+    let result = slopes.iter()
+        .map(|(x,y)| check_slope(&x,&y, &parsed_lines))
+        .fold(1usize, | curr, next | curr * next);
+
+    println!("{}", result)
+}
+
+fn check_slope(slope_x: &usize, slope_y: &usize, lines: &Vec<Vec<usize>>) -> usize {
     let mut x = 0;
     let mut y = 0;
 
-    let max_y = parsed_lines.len();
-    let max_x = parsed_lines.first().unwrap().len();
+    let max_y = lines.len();
+    let max_x = lines.first().unwrap().len();
 
-    let mut result = 0;
+    let mut result = 0usize;
 
     while y < max_y {
-        let line = &parsed_lines[y];
+        let line = &lines[y];
 
         result += line[x];
 
-        y += 1;
-        x += 3;
+        y += slope_y;
+        x += slope_x;
         if x >= max_x {
             x = x - max_x
         }
     }
-
-    println!("{}", result);
+    return result;
 }
